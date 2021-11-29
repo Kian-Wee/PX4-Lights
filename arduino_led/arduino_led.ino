9942b8342730040e12446b2e0420a3b8a95ca030
Kian Wee <kianweetan2002@gmail.com>
2021-11-29 17:41:27 +0800

    modified:   arduino_led/arduino_led.ino 	modified:   buzzer_ros.py

--- arduino_led/arduino_led.ino
+++ arduino_led/arduino_led.ino
@@ -1,13 +1,20 @@
-const int L=0; //Left LED and speaker
+const int L=0; //Left LED
 const int R=1; //Right LED
+const int S=2; //Speaker
+const int LED=3; //WS2812B LED DIGITAL PIN
 int incomingByte;
 int previousByte;
+int tLOW=0; //timer variable
+int tHIGH=0;
+int tLED=0;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(L, OUTPUT);
   pinMode(R, OUTPUT);
-  Serial.begin(9600);
+  pinMode(S, OUTPUT);
+  pinMode(LED, OUTPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
@@ -15,6 +22,7 @@ void loop() {
     // read the oldest byte in the serial buffer:
     incomingByte = Serial.read();
     previousByte = incomingByte;
+    previousByte[0];
     // if it's a capital H (ASCII 72), turn on the LED:
     if (incomingByte == 'H') {
       digitalWrite(0, LOW);
--- buzzer_ros.py
+++ buzzer_ros.py
@@ -3,39 +3,85 @@ import rospy
 from std_msgs.msg import Int32
 import serial
 import time
+from mavros_msgs.msg import State
+from sensor_msgs.msg import BatteryState
 
 # Serial connection to teensy
 #ser = serial.Serial('/dev/ttyUSB0', 9600)
 #ser = serial.Serial('COM10', 9600) 
-ser = serial.Serial('/dev/serial/by-id/usb-Raspberry_Pi_PicoArduino_DF6050A04B711139-if00', 9600)
+ser = serial.Serial('/dev/serial/by-id/usb-Raspberry_Pi_PicoArduino_DF6050A04B711139-if00', 115200)
 
 ## TODO, ADD CHANNEL 5 BYPASS
 
 time.sleep(2) # wait for the serial connection to initialize
 
+previnput=''
+HRI=0
+armed=None
+flightmode=None
+battery=None
+
+batterymin=3.2
+batterymax=4.2
+
 # Turns on buzzer when user input is on and turns off buzzer when user input is off
 # Currently latches on to the previous input, ie requires input 0 to turn it off
-def callback(msg):
-    user_input = msg.data
-    
-    if user_input >0:
+def processor():
+
+    global previnput, batterymin, batterymax
+    user_input=HRI
+
+    sendstring = '' #COMBINE BATTERY WITH FLIGHT MODE to reduce serial load
+
+    batterylevel= (battery-batterymin)/(batterymax-batterymin)
+    batterylevel= round(batterylevel/10)*10
+    sendstring+=batterylevel
+
+    if armed==False:
+        sendstring+='D'
+    elif user_input >0:
         print("Alarm is on")
-        ser.write(b'H') 
+        sendstring+='H'
     elif user_input ==0:
         print("Alarm is off")
-        ser.write(b'L')
+        sendstring+='L'
     elif user_input <0:
         print("Program Exiting")
-        ser.write(b'L')
+        sendstring+='L'
         ser.close()
-    else:
-        print("Invalid")
+    elif flightmode=='OFFBOARD':
+        sendstring+='O'
+    elif flightmode=='STABILIZED':
+        sendstring+='S'
+
+    if user_input != previnput:
+        ser.write(sendstring)
+
+    previnput=sendstring
+
+
+
+def callback(msg):
+    global HRI
+    HRI = msg.data
+
+def statecallback(msg):
+    global flightmode, armed
+    flightmode = msg.mode
+    armed = msg.armed
+
+def batterycallback(msg):
+    global battery
+    batterytemp=msg.cellvoltage
+    battery=sum(batterytemp)/len(batterytemp)
 
 if __name__ == '__main__':
     rospy.init_node('alarm') 
-    sub = rospy.Subscriber("/hri_user_input", Int32, callback)
+    hrisub = rospy.Subscriber("/hri_user_input", Int32, callback)
+    statesub = rospy.Subscriber("uav0/mavros/state", State, statecallback)
+    batterysub = rospy.Subscriber("uav0/mavros/battery", BatteryState, batterycallback)
     rospy.spin()
 
 
-#Test
+#Test Publish
 #rostopic pub /hri_user_input std_msgs/Int32 1
