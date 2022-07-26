842741f941c270a9580d29b2660d0a4a7c74a473
Kian-Wee <kianweetan2002@gmail.com>
2022-07-26 12:43:07 +0800

    updated code

--- arduino_led/arduino_led.ino
+++ arduino_led/arduino_led.ino
@@ -1,5 +1,3 @@
-//TODO, CHANGE BUZZER TO NOT INTERRUPTING
-
 #include "src/FastLED/src/FastLED.h"
 //#include "FastLED.h"
 #define NUM_LEDS 4
@@ -29,17 +27,6 @@ int tLED=0;
 int tcur=0;
 
 String incomingByte; //Using .readstring currently, use int(ascii) for .read
-
-struct timer
-{
-  public:
-    unsigned long high;
-    unsigned long low;
-};
-
-timer Alarm = {
-  0,0
-};
 
 void setup() {
   pinMode(LB, OUTPUT);
@@ -101,12 +88,8 @@ void readSerial(){
      AlarmState=0;
    }
 
-//   if (String(incomingByte[1]) == "S"){
-//    FlightState=2;
-//   }
-
     String batterylevel = String(incomingByte);
-    batterylevel.remove(0,2); //Remove flight mode to leave string with batterylevel
+    batterylevel.remove(0,1); //Remove flight mode to leave string with batterylevel
     batterylevelint = batterylevel.toInt();
     printto("Battery level is: "+String(batterylevelint));
   }
@@ -118,48 +101,9 @@ void flightmode(){
   extern String FlightState;
   extern String prevFlightState;
   
-  if(FlightState=="H"){
-    printto("Alarm On");
-    // if (millis()>Alarm.low){ //Sequence has finished
-    //   Alarm.high=rand()%200+10+millis(); // Regenerate new timing variables
-    //   Alarm.low=rand()%200+10+Alarm.high;
-    // }else if (millis()<Alarm.high){
-    //   digitalWrite(L, LOW);
-    //   digitalWrite(R, HIGH);
-    //   digitalWrite(S, HIGH);
-    //   for(int i=0; i<15; i++) leds[i]=CRGB::Red;
-    // }else if (millis()<Alarm.low){
-    //   digitalWrite(L, LOW);
-    //   digitalWrite(R, HIGH);
-    //   digitalWrite(S, LOW);
-    // }
-    lightup(CRGB::Yellow,0);
-    FastLED.show();
-    
-    digitalWrite(LB, LOW);
-    digitalWrite(L, LOW);
-    digitalWrite(RB, HIGH);
-    digitalWrite(R, HIGH);
-    digitalWrite(S, HIGH);
-    delay(rand()%200+10);
-    digitalWrite(RB, LOW);
-    digitalWrite(R, LOW);
-    digitalWrite(LB, HIGH);
-    digitalWrite(L, HIGH);
-    digitalWrite(S, LOW);
-    delay(rand()%200+10);
-    
-  
-  }else if(FlightState != prevFlightState){
+  if(FlightState != prevFlightState){
 
-    if(FlightState=="R"){
-        printto("Scout mode");
-        digitalWrite(LB, HIGH);
-        digitalWrite(L, HIGH);
-        digitalWrite(RB, HIGH);
-        digitalWrite(R, HIGH);
-        lightup(CRGB::Orange, 0);
-    }else if(FlightState=="O"){
+    if(FlightState=="O"){
         printto("Offboard mode");
         digitalWrite(LB, LOW);
         digitalWrite(L, LOW);
@@ -183,16 +127,6 @@ void flightmode(){
         digitalWrite(R, LOW);
         digitalWrite(S, LOW);
         lightup(CRGB::Black, 0);
-  // This is only used when mavros is not turned on and the alarm needs to be off
-  }else if(FlightState=="L"){
-        printto("Alarm off");
-        digitalWrite(LB, LOW);
-        digitalWrite(L, LOW);
-        digitalWrite(RB, LOW);
-        digitalWrite(R, LOW);
-        digitalWrite(S, LOW);
-        lightup(CRGB::Black, 0);
-  }
   prevFlightState=FlightState;
   }else{
       digitalWrite(LB, LOW);
@@ -204,27 +138,7 @@ void flightmode(){
   }
   
 }
-
-
-void plannermode(){
-  if(PlannerState != prevPlannerState){
-
-    if(PlannerState=="F"){
-        lightup(CRGB::Yellow, 1);
-    }else if(PlannerState=="A"){
-        lightup(CRGB::MediumOrchid, 1);
-    }else if(PlannerState=="M"){
-        lightup(CRGB::Green, 1);
-    }else if(PlannerState=="I"){
-        lightup(CRGB::Black, 1);
-  }
-  prevPlannerState=PlannerState;
-  }else{
-      lightup(CRGB::Black, 1);
-  }
-  
 }
-
 
 void lightup(CRGB color, bool mode){
   for(int i=0; i<NUM_LEDS; i++){
@@ -235,7 +149,6 @@ void lightup(CRGB color, bool mode){
     
   }
   }
-
 }
 
 void loop() {
@@ -243,139 +156,8 @@ void loop() {
  printto("looping");
  readSerial();
  flightmode();
- plannermode();
  FastLED.show();
- delay(1000);
+ delay(100); // To prevent microcontroller from restarting due to fastled.show
 
-//batterydisplay(batterylevelint); //Send the number of LEDs(1-10) to light up
-
- //delay(500); // To prevent microcontroller from restarting due to fastled.show
-
-//   if (Serial.available() > 0) {
-//    // read the oldest byte in the serial buffer:
-//    incomingByte = Serial.readStringUntil('\n');
-////    previousByte = incomingByte;
-//    // if it's a capital H (ASCII 72), turn on the LED:
-//      if(incomingByte== "S5"){
-//        Serial.println("HIGH");
-//        digitalWrite(10,HIGH);
-//        fill_solid(leds, NUM_LEDS, CRGB::Red);
-//        FastLED.show();
-//      }
-//      if(incomingByte== "R10"){
-//        Serial.println("LOW");
-//        digitalWrite(10,LOW);
-//        fill_solid(leds, NUM_LEDS, CRGB::White);
-//        FastLED.show();
-//      }
-//      Serial.print("I received: ");
-//      Serial.println(incomingByte);
-//   }
-
-//  uint16_t timerA = 500;  // How often to run Event A [milliseconds]
-//  uint16_t timerB = 500;  // How long after A to run Event B [milliseconds]
-//
-//
-//  // Setting the amount of time for "triggerTimer".
-//  // You can name "triggerTimer" whatever you want.
-//  static CEveryNMilliseconds triggerTimer(timerB);
-//  
-//  EVERY_N_MILLISECONDS(timerA){
-//    // do Event A stuff
-//    fill_solid(leds, NUM_LEDS, CHSV(random8(),255,128));
-//    counterTriggered = 1;  // Set to True
-//    triggerTimer.reset();  // Start trigger timer
-//  }
-//  
-//  if (counterTriggered == 1) {  // Will only be True if Event A has started
-//    if (triggerTimer) {  // Check if triggerTimer time reached
-//      // do Event B stuff
-//      for (uint8_t i=0; i<NUM_LEDS/2; i++){
-//        leds[random8(NUM_LEDS)] = CRGB::Red;
-//      }
-//      counterTriggered = 0;  // Set back to False
-//    }
-//  }
-//
-//
-//  EVERY_N_SECONDS_I( timingObj, 20) {
-//    // This initally defaults to 20 seconds, but then will change the run
-//    // period to a new random number of seconds from 10 and 30 seconds.
-//    // You can name "timingObj" whatever you want.
-//    timingObj.setPeriod( random8(10,31) );
-//    FastLED.clear();
-//    for (uint16_t i=0; i<NUM_LEDS*3; i++){
-//      leds[random8(NUM_LEDS)] = CRGB::Black;
-//      leds[random8(NUM_LEDS)] = CHSV(random8(), random8(140,255), random8(50,255));
-//      FastLED.show();
-//      delay(random8(20,80));
-//    }
-//    delay(500);
-//  }
-
-    
+  //batterydisplay(batterylevelint); //Send the number of LEDs(1-10) to light up
 }
-
-// void test(){
-
-//   Serial.println("Testing every LED");
-//   FastLED.clear();
-//   for(int i=0;i<NUM_LEDS; i++){
-//     leds[i]=CRGB::Red;
-//     FastLED.show();
-//     delay(50);
-//   }
-//   delay(2000);
-//   FastLED.clear();
-  
-//   Serial.println("Testing Battery");
-//   // Run through the 10 LEDS assigned to battery
-//   for(int i=0; i<11; i++){
-//     Serial.print("Displaying battery at ");
-//     Serial.println(i*10);
-//     batterydisplay(i);
-//     FastLED.show();
-//     delay(50);
-//   }
-//   delay(2000);
-//   FastLED.clear();
-
-//   extern String FlightState;
-//   Serial.println("Testing Flight Modes");
-
-//   FlightState="H";
-//   Serial.print("test ");
-//   Serial.println(FlightState);
-//   flightmode();
-//   batterydisplay(10);
-//   FastLED.show();
-//   delay(1000);
-//   FastLED.clear();
-  
-//   FlightState="R";
-//   Serial.print("test ");
-//   Serial.println(FlightState);
-//   flightmode();
-//   batterydisplay(5);
-//   FastLED.show();
-//   delay(1000);
-//   FastLED.clear();
-
-//   FlightState="O";
-//   Serial.print("test ");
-//   Serial.println(FlightState);
-//   flightmode();
-//   batterydisplay(10);
-//   FastLED.show();
-//   delay(1000);
-//   FastLED.clear();
-
-//   FlightState="S";
-//   Serial.print("test ");
-//   Serial.println(FlightState);      
-//   flightmode();
-//   batterydisplay(5);
-//   FastLED.show();
-//   delay(1000);
-
-// }
--- buzzer_ros.py
+++ buzzer_ros.py
@@ -18,10 +18,9 @@ ser = serial.Serial('/dev/serial/by-id/usb-Raspberry_Pi_PicoArduino_DF6050A04B2D
 #ser = serial.Serial('/dev/serial/by-id/usb-Raspberry_Pi_PicoArduino_DF6050A04B1A4E39-if00', 115200) #1
 #ser = serial.Serial('/dev/serial/by-id/usb-Espressif_USB_JTAG_serial_debug_unit_84:F7:03:A0:FC:F0-if00', 115200)
 
-time.sleep(2) # wait for the serial connection to initialize
+time.sleep(1) # wait for the serial connection to initialize
 
 drone_name = os.getenv('DRONE_NAME') #environment variable set in bashrc(or exported in terminal)
-
 
 batterymin=3.2
 batterymax=4.2
@@ -31,10 +30,8 @@ batterymax=4.2
 class processor():
 
     def __init__(self):
-        rospy.Subscriber(drone_name + "/hri_mode", String, self.modecallback)
         rospy.Subscriber(drone_name + "/mavros/state", State, self.statecallback)
         rospy.Subscriber(drone_name + "/mavros/battery", BatteryState, self.batterycallback)
-        rospy.Subscriber(drone_name + "/planner_staus",  String, self.plannerstatecallback)
 
         self.previnput=''
         self.usermode=''
@@ -53,12 +50,6 @@ class processor():
 
             if self.armed==False:
                 self.sentstring+='D'
-            elif self.usermode =="Distract":
-                # print("Alarm is on")
-                self.sentstring+='H'
-            elif self.usermode =="Sweep":
-                # print("Scout mode")
-                self.sentstring+='R'
             elif self.flightmode=='OFFBOARD':
                 # print("Offboard mode")
                 self.sentstring+='O'
@@ -68,15 +59,6 @@ class processor():
             else:
                 #print("Disarming")
                 self.sentstring+='D'
-                
-            if self.plannerstate =="Formation":
-                self.sentstring+='F'
-            elif self.plannerstate =="S&A":
-                self.sentstring+='A'
-            elif self.plannerstate =="Moving":
-                self.sentstring+='M'
-            else: # Idle
-                self.sentstring+='I'
 
             batterylevel= (self.battery-batterymin)/(batterymax-batterymin)
             batterylevel= round(batterylevel*10)
@@ -85,10 +67,6 @@ class processor():
             if self.sentstring != self.previnput:
                 print("Sending to ESP32",self.sentstring)
                 ser.write(self.sentstring)
-
-            # #for testing
-            # print("Sending to Microcontroller",self.sentstring)
-            # ser.write(self.sentstring)
 
             self.previnput=self.sentstring
 
@@ -100,9 +78,6 @@ class processor():
         ser.close()
         # rospy.signal_shutdown("BUZZER SHUTTING DOWN")
 
-    def modecallback(self,msg):
-        self.usermode = msg.data
-
     def statecallback(self,msg):
         self.flightmode = msg.mode
         self.armed = msg.armed
@@ -113,9 +88,6 @@ class processor():
             self.battery = 0
         else:
             self.battery=sum(batterytemp)/len(batterytemp)
-
-    def plannerstatecallback(self,msg):
-        self.plannerstate=msg.data
 
 if __name__ == '__main__':
     rospy.init_node(drone_name + '_lights') 
